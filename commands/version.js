f51f4e8f71a646d366b93537306cd05e00797b31
Michael Ormsbee <michael.o@zeamster.com>
2020-10-13 02:56:12 -0400

    Version 1.0.0

---
+++ commands/version.js
@@ -0,0 +1,8 @@
+async function run(context) {
+  // print out the version of your plugin package
+  context.print.info('version 1.0.0');
+}
+
+module.exports = {
+  run,
+};
---
+++ event-handlers/handle-PrePush.js
@@ -0,0 +1,66 @@
+const eventName = 'PrePush';
+
+async function run(context, args) {
+  // insert your code to handle the amplify cli PrePush event
+  context.print.info(`Event handler ${eventName} to be implemented.`);
+
+  console.log(context);
+
+  let cftloc = "./amplify/backend/api/atlasAPI/build/cloudformation-template.json";
+  console.log(cftloc);
+
+  let cft = context.filesystem.read(cftloc);
+
+  if (!cft) {
+    console.log('ERROR! NO Cloudformation Template found.');
+    return;
+  }
+
+  let cftJson = JSON.parse(cft);
+  let cftResources = cftJson.Resources;
+  let item, dependOnName = "";
+
+  for (item in cftResources) {
+    if (
+      cftResources.hasOwnProperty(item)
+      && item != "ConnectionStack"
+      && item != "SearchableStack"
+      && cftResources[item].Properties
+      && cftResources[item].Properties.Parameters
+      && cftResources[item].Properties.Parameters.DynamoDBModelTableReadIOPS
+    ) {
+        if (dependOnName != "") {
+
+          //if (typeof cftResources[item].DependsOn[dependOnName] == "undefined") {
+            let dependsOn = Object.values(cftResources[item].DependsOn);
+            dependsOn.push(dependOnName);
+            cftJson.Resources[item].DependsOn = dependsOn;
+            console.log("Adding " + dependOnName + " to "+ item + ".DependsOn...........");
+
+          // } else {
+          //   console.log(dependOnName + ".DependsOn already contains "+ item + "...........");
+          // }
+          // console.log(cftJson.Resources[item].DependsOn);
+
+
+        } else {
+          console.log("Skipping first table - " + item + "..............");
+        }
+        dependOnName = item;
+    }
+  }
+
+  let result = cftJson;
+  if (context.parameters.options.minify === true) {
+    result = JSON.stringify(result, null, 0);
+  } else {
+    result = JSON.stringify(result, " ", 2);
+  }
+  console.log(result)
+
+  context.filesystem.write(cftloc, result);
+}
+
+module.exports = {
+  run,
+};
